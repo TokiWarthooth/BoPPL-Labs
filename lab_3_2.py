3fa94e42b619dc3b37d9b058ac23a95b099e6be7
skwigelf <max.great339@gmail.com>
2025-09-05 19:59:00 +0300

    Задание №2. Напишите функцию, которая принимает неограниченное количество целых чисел (в качестве аргументов) и возвращает их среднеарифметическое значение.

---
+++ lab_3_2.py
@@ -0,0 +1,27 @@
+# Author: Skwisgaar Skwigelf aka Skripnikov Maxim <max.great339@gmail.com>
+# Date: 05.09.2025
+
+import time
+import math
+
+print('Счетчик среднего арифмитического')
+time.sleep(1)
+
+def main():
+    time.sleep(1)
+    numbers = input('Введите числа через пробел: ')
+    numbers = list(map(int, numbers.split()))
+    result = process(*numbers)
+    print(result)
+
+# счетчик суммы
+def process(*numbers):
+    total = 0
+    for num in numbers:
+        total += num
+
+    count = len(numbers)
+    result = total / count
+    return result
+
+main()
